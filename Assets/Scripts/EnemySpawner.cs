543788e5d91b6c7ad061640b6b55d01bcd0b9720
juandavid03 <espinosajuan3@msn.com>
2018-01-31 13:35:36 -0500

    Enemy Spawner
    Enemy spawner funcionando, y dos prefabs mas (2 aliens)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+
+    public float tiempo;
+
+    public int index;
+
+    // Use this for initialization
+    void Start ()
+    {
+        tiempo = UnityEngine.Random.Range(1, 10);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        tiempo -= Time.deltaTime;
+        if (tiempo <= 0)
+        {
+            tiempo = UnityEngine.Random.Range(1, 10);
+            InstantiateEnemy();
+
+        }
+	}
+
+    void InstantiateEnemy()
+    {
+        index = UnityEngine.Random.Range(0, enemies.Length);
+        Instantiate(enemies[index]);
+        StartCoroutine(enableSpawnCR());
+    }
+
+    private IEnumerator enableSpawnCR()
+    {
+        yield return new WaitForSeconds(tiempo);
+
+    }
+}
