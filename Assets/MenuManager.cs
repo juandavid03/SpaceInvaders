c042f1a3b5ff2eacbb98dad62df7a21283c1c638
juandavid03 <espinosajuan3@msn.com>
2018-01-31 12:04:29 -0500

    Menus
    Creacion de las distintas escenas y el "MenuManager" (codigo para el uso de los botones del menu principal) funcionando.

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.K) && SceneManager.GetActiveScene().name == "Tutorial")
+        {
+            Debug.Log("QUIERO CAMBIAR AL JUEGO");
+            LoadScene("Nivel1");
+        }
+    }
+    public void LoadScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
