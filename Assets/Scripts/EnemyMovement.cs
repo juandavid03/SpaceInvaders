e52aa9cfca0f19f1d0a5d6213153e83350a76ce6
juandavid03 <espinosajuan3@msn.com>
2018-01-31 18:50:08 -0500

    Movimiento de enemigos
    -Los enemigos se mueven con un patron basico (izquierda a derecha).

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public bool isGoingRight;
+
+    public Rigidbody2D enemigo;
+
+    public int direccion;
+
+    public int fuerza;
+
+	// Use this for initialization
+	void Start ()
+    {
+        enemigo = this.gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        enemigo.AddForce(new Vector2(direccion * fuerza, 0));
+        if (isGoingRight)
+        {
+            direccion = 1;
+        }
+        else if (!isGoingRight)
+        {
+            direccion = -1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        enemigo.transform.position += new Vector3(0, -1, 0);
+        if (collision.gameObject.CompareTag("Side"))
+        {
+            isGoingRight = !isGoingRight;
+        }
+    }
+}
