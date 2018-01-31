4777d206d43a6c5fb8f6e3efe8be31b169ffbeba
juandavid03 <espinosajuan3@msn.com>
2018-01-31 12:17:13 -0500

    Movimiento basico de la nave
    -Creacion de la clase nave.
    -Movimiento basico de la clase nave (derecha e izquierda) funcionando.

---
+++ Assets/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private Rigidbody2D nave;
+
+    public int force;
+	// Use this for initialization
+	void Start ()
+    {
+        nave = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            nave.AddForce(new Vector2(-force, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            nave.AddForce(new Vector2(force, 0));
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            nave.AddForce(new Vector2(force, 0));
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            nave.AddForce(new Vector2(-force, 0));
+        }
+    }
+}
