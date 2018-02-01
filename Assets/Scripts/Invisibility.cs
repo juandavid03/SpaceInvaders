b4a7ed06ac8e772eb944af1d3c9ba7f958cb0318
juandavid03 <espinosajuan3@msn.com>
2018-01-31 20:37:50 -0500

    Alien 3
    -Prefab de alien 3
    -Creacion de funcionalidad de componente de invisibilidad de enemigo 3

---
+++ Assets/Scripts/Invisibility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisibility : MonoBehaviour
+{
+    private bool isVisible = true;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(ChangeVisibilityCR());
+	}
+
+    private IEnumerator ChangeVisibilityCR()
+    {
+        if (isVisible)
+        {
+            yield return new WaitForSeconds(2);
+            isVisible = !isVisible;
+        }
+        if (!isVisible)
+        {
+            yield return new WaitForSeconds(1);
+            isVisible = !isVisible;
+        }
+
+        StartCoroutine(ChangeVisibilityCR());
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isVisible)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else if (!isVisible)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = false; ;
+        }
+    }
+}
