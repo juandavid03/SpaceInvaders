70557928d7d96842d6899894cbcb5c6a6d881d98
juandavid03 <espinosajuan3@msn.com>
2018-01-31 22:43:27 -0500

    Escena tutorial y correcciones
    -Escena de tutorial creada
    -Cambios al menuManager para que funcione en todas las escenas

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,9 +6,13 @@ public class MenuManager : MonoBehaviour
     public void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.K) && SceneManager.GetActiveScene().name == "Tutorial")
+        if (Input.GetKeyDown(KeyCode.K) && (SceneManager.GetActiveScene().name == "Tutorial" || SceneManager.GetActiveScene().name == "Highscore"))
         {
             LoadScene("Nivel1");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && (SceneManager.GetActiveScene().name == "Tutorial" || SceneManager.GetActiveScene().name == "Highscore"))
+        {
+            LoadScene("MainMenu");
         }
     }
     public void LoadScene(string nextScene)
